4bf255648a184ddd75903c5d54f69126a84ea7c2
arao <araobp@users.noreply.github.com>
2024-04-21 09:11:25 +0900

    update

--- node/README.md
+++
@@ -1,5 +0,0 @@
-# Node.js SEA
-
-## Reference
-
-- [How To Create a Node.js Module](https://www.digitalocean.com/community/tutorials/how-to-create-a-node-js-module)
--- node/test.sh
+++
@@ -1,7 +0,0 @@
-npm uninstall browser-utils
-npm uninstall db
-rm -rf node_modules/browser-utils
-rm -rf node_modules/db
-npm install ./browser-utils
-npm install ./db
-node main.js
